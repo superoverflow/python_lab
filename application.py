68d553d60c5991b4e81b0cd3da20b6722e0529aa
Superoverflow <cyrus.tang@superoverflow.com>
2017-01-28 21:32:58 +0000

    update requirements

--- application.py
+++ application.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask.ext.mongoengine import MongoEngine
+
+db = MongoEngine()
 
 def create_app():
     app = Flask(__name__)
     from tasks.views import tasks_app
     app.register_blueprint(tasks_app)
+    db.init_app(app)
     return app
